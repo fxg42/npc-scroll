44ebf249fca0b5439d58e3de644df545bdd28251
Francois-Xavier Guillemette <francois.x.guillemette@gmail.com>
2021-06-30 21:21:12 -0400

    Adds cantrip autoincrement for spellcasters

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ to change the some "offense" abilities in the combat tracker.
 
 ### How to
 
-On the combat tracker, while holding the **Ctrl** key and hovering an Offence ability, use the mouse
-wheel to increment or decrement either:
+On the combat tracker, while holding the **Ctrl** key and hovering an Offence
+ability, use the mouse wheel to increment or decrement either:
 - the attack bonus;
 - the number of damage or healing dice;
 - the save DC;
@@ -15,6 +15,13 @@ wheel to increment or decrement either:
 Hold the **Alt** key and use the mouse wheel to cycle through:
 - the damage and healing die type (d4, d6, d8, d10 or d12);
 - the saving throw's ability (str, dex, con, int, wis, cha).
+
+### Automatic cantrip increment for spellcasters
+
+When added to the CT, NPC's with the "Spellcasting" trait will have their
+cantrip's DMG and HEAL die expressions automatically incremented based on their
+spellcasting level. Dice count is incremented once if 5th-level or above, twice
+starting at 11th-level and three times after 17th.
 
 ### Inspiration
 
--- scripts/manager_combat2.lua
+++ scripts/manager_combat2.lua
@@ -1,29 +1,92 @@
 function onInit()
-  -- Proxy CombatManage.addNPC function
-	CombatManager.addNPC = addNPC(CombatManager.addNPC);
+  CombatManager.addNPC = proxy(CombatManager.addNPC, upcastCantrips);
 end
 
-function addNPC(subjectFn)
-  return function(sClass, nodeNPC, sName)
-    local nodeEntry = subjectFn(sClass, nodeNPC, sName);
+--
+-- Intercepts calls to subject function and executes an advice after the call.
+--
 
-    local spellcasterLevel = parseSpellcasterLevel(nodeEntry);
-    if spellcasterLevel then
-      Debug.console(spellcasterLevel)
-    end
-
-    return nodeEntry;
+function proxy(subjectFn, afterAdvice)
+  return function(...)
+    local returnValue = subjectFn(...);
+    afterAdvice(returnValue);
+    return returnValue;
   end
 end
 
-function parseSpellcasterLevel(nodeEntry)
+--
+-- Gets the spellcaster level from the "Spellcasting" Trait then loops through
+-- cantrips while incrementing the number of DMG or HEAL dice.
+--
+
+function upcastCantrips(nodeEntry)
+  local notches = getNbNotches(getSpellcasterLevel(nodeEntry));
+  if notches < 1 then return end
+  
+  forEachCantrip(nodeEntry, function(attackLine, setValue)
+    if attackLine:find("DMG:") or attackLine:find("HEAL:") then
+      setValue(updateNbDice(notches, attackLine));
+    end
+  end);
+end
+
+--
+-- Reads the nodeEntry's "Spellcasting" trait and returns the spellcaster
+-- level or 0.
+--
+
+function getSpellcasterLevel(nodeEntry)
   for _, nodePower in pairs(DB.getChildren(nodeEntry, "traits")) do
     local name = StringManager.trim(DB.getValue(nodePower, "name", ""):lower());
-    if name == "spellcasting"then
+    if name == "spellcasting" then
       local desc = DB.getValue(nodePower, "desc", "");
       local level = desc:match("(%d+)..-level spellcaster") or 0;
       return tonumber(level);
     end
   end
-  return nil;
+  return 0;
+end
+
+--
+-- Determines the number of times ("notches") cantrips should be incremented
+-- based on the spellcaster's level. Uses standard thresholds of 5th, 11th
+-- and 17th level.
+--
+
+function getNbNotches(spellcasterLevel)
+  local steps = { 17, 11, 5, 0 };
+  for idx, limit in ipairs(steps) do
+    if spellcasterLevel >= limit then
+      return #steps - idx;
+    end
+  end
+  return 0;
+end
+
+--
+-- Loops through all cantrips found in the nodeEntry and calls function "f"
+-- for each, passing it the attack line and a setValue callback to modify the
+-- entry's power in the database.
+--
+
+function forEachCantrip(nodeEntry, f)
+  for _, spellTrait in pairs({"spells", "innatespells"}) do
+    for _, nodePower in pairs(DB.getChildren(nodeEntry, spellTrait)) do
+      local attackLine = DB.getValue(nodePower, "value", "");
+      if attackLine:find("cantrip") then
+        local setValue = function(...) DB.setValue(nodePower, "value", "string", ...) end
+        f(attackLine, setValue);
+      end
+    end
+  end
+end
+
+--
+-- Attack line updaters
+--
+
+function updateNbDice(notches, attackLine)
+  return attackLine:gsub("(-?%d+)d(%d+)", function(qty, die)
+    return string.format("%dd%s", math.max(1, (tonumber(qty) + notches)), die);
+  end);
 end
